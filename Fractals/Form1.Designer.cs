8188c6c0669fa43782da98926d597e1fa237ba73
13stulev <52106670+13stulev@users.noreply.github.com>
2024-01-25 14:00:41 +0400

    Added project

---
+++ Fractals/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1784, 812);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(0, 818);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1478, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.Draw);
+            this.trackBar1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Draw);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1784, 861);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Fractals/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractals
+{
+    public partial class Form1 : Form
+    {
+        public float modifier = 1;
+        public string rules = "";
+        static void setRule(int n, ref string result)
+        {
+            result = "XF";
+            for (int i = 0; i < n; i++)
+            {
+                result = result.Replace("X", "X+yF++yF-FX--FXFX-yF+");
+                result = result.Replace("Y", "-FX+YFYF++YF+FX--FX-Y");
+                result = result.Replace("y", "Y");
+            }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }    
+          private void Draw(object sender, EventArgs e)
+          {
+        
+              Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        
+              Pen blackPen = new Pen(Color.Black, 2);
+        
+              setRule(trackBar1.Value, ref rules);
+        
+              // Create points that define line.
+              PointF point1 = new PointF(pictureBox1.Width - 700, pictureBox1.Height - 700);
+              PointF point2 = new PointF(pictureBox1.Width - 700, pictureBox1.Height - 700);
+              
+              using (var graphics = Graphics.FromImage(bmp))
+              {
+                  int h = 6;
+                  int angle = 0;
+                  foreach (char c in rules)
+                  {
+                      switch (c)
+                      {
+                          case 'F':
+                              point2.X = point2.X + (float)(h * Math.Cos(angle * Math.PI / 180));
+                              point2.Y = point2.Y + (float)(h * Math.Sin(angle * Math.PI / 180));
+                              graphics.DrawLine(blackPen, point1, point2);
+                              point1.X = point2.X;
+                              point1.Y = point2.Y;
+                              break;
+                          case '+':
+                              angle += 60;
+                              break;
+                          case '-':
+                              angle -= 60;
+                              break;
+                      }
+                  }
+                  
+              }
+              pictureBox1.Image = bmp;
+          }
+    }
+}
